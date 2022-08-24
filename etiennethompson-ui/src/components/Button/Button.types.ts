e25eb84829dcda17a2e1a62c53721e47ba34115e
Etienne Thompson <et@etiennethompson.com>
2022-08-23 18:55:30 -0700

    Add basic button component code.

--- etiennethompson-ui/src/components/Button/Button.types.ts
+++ etiennethompson-ui/src/components/Button/Button.types.ts
@@ -1,9 +1,9 @@
-import { MouseEventHandler } from "react";
+import React, { MouseEventHandler } from "react";
 
 export interface ButtonProps {
   className?: string;
-  id?: string;
-  text?: string;
+  children?: React.ReactNode;
   disabled?: boolean;
+  id?: string;
   onClick?: MouseEventHandler<HTMLButtonElement>;
 }
