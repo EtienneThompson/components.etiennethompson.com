d567b3d64adca4b255560cb0a01aa081eea0b1cb
Etienne Thompson <et@etiennethompson.com>
2022-09-14 20:37:02 -0700

    Add simple toolbar component for navigating between components.

--- doc-site/src/App.tsx
+++ doc-site/src/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import "./App.scss";
+import { Toolbar } from "./components/Toolbar/Toolbar";
 
 function App() {
   // document.documentElement.className = "theme-light";
 
   return (
     <div className="App">
+      <Toolbar />
+      <div style={{ marginTop: "50px" }}></div>
       <Outlet />
     </div>
   );
